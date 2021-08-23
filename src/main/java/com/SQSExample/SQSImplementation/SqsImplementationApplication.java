5ed8f45897a8b71a9a569db5113b36c8e86a1dd9
therageofshiva <skamaleshfg@gmail.com>
2021-08-23 15:10:15 +0530

    A simple AWS SQS Listener Implementation.
    hit the http://localhost:10091/sqs/send url with a message for
    consumption.

---
+++ src/main/java/com/SQSExample/SQSImplementation/SqsImplementationApplication.java
@@ -0,0 +1,18 @@
+package com.SQSExample.SQSImplementation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsImplementationApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SqsImplementationApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsImplementationApplication.class, args);
+		LOGGER.info("Springboot with Amazon SQS service started successfully");
+	}
+
+}
